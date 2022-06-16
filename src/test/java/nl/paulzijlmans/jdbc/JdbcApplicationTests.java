d59b60751a9e9dcbbd9a7fed5147ed5de9ab97a2
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-16 08:41:33 +0200

    Use Testcontainers jdbc url support

--- src/test/java/nl/paulzijlmans/jdbc/JdbcApplicationTests.java
+++ src/test/java/nl/paulzijlmans/jdbc/JdbcApplicationTests.java
@@ -6,25 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 class JdbcApplicationTests {
-
-  @Container
-  public static PostgreSQLContainer<?> psql = new PostgreSQLContainer<>("postgres:12");
-
-  @DynamicPropertySource
-  static void configureTestcontainersProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", psql::getJdbcUrl);
-    registry.add("spring.datasource.username", psql::getUsername);
-    registry.add("spring.datasource.password", psql::getPassword);
-  }
 
   @Autowired
   public CustomerRepository customerRepository;
